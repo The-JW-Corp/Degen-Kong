be0b247b9f0cd14a883b7b4ab0d3d77eb9819859
Ra <rami@jwcorp.fr>
2023-12-26 09:37:13 +0100

    style: add lottie + button + rarity page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,8 +1,10 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-    Degenkong
+      Degenkong
+      <Link href="/rarity">Rarity page</Link>
     </>
-  )
+  );
 }
---
+++ client/src/app/rarity/page.tsx
@@ -0,0 +1,13 @@
+import Button from "@/components/Button.jsx";
+import React from "react";
+
+async function page() {
+  return (
+    <div>
+      rarity page
+      <Button />
+    </div>
+  );
+}
+
+export default page; 
