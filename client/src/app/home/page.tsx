0bc02382bf8cd0f3000e65dc781340201ddec0c0
Ra <rami@jwcorp.fr>
2023-12-31 11:52:02 +0100

    style: home page

--- client/src/app/home/page.tsx
+++ client/src/app/home/page.tsx
@@ -1,14 +1,14 @@
-import Button from "@/components/Button";
-import Link from "next/link";
-
+import Image from "next/image";
+import WatermarkTitle from "../../../public/Assets/image/degen-kong-watermark-title.svg";
+import degenKongLogo from "../../../public/Assets/image/degen-kong-logo.svg"
+import styles from "../../styles/home.module.css";
 export default function Home() {
   return (
     <>
-      Home
-      {/* <Link href="/rarity">Rarity page</Link>
-      <Button text="home" />
-      <Button text="stake" />
-      <Button text="rarity" /> */}
+      <div className={styles.page_container}>
+        <Image src={degenKongLogo} quality={100} width={450} height={450} />
+        <Image src={WatermarkTitle} quality={100} />
+      </div>
     </>
   );
 }
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,32 +7,40 @@ import Image from "next/image.js";
 import { usePathname } from "next/navigation";
 function Navbar() {
   const [activeButton, setActiveButton] = useState(null);
-  const [isWalletConnected, setIsWalletConnected] = useState(true);
-  const buttonsName = ["home", "stake", "roadmap", "rarity", "connect wallet"];
+  const [isWalletConnected, setIsWalletConnected] = useState(false);
+  const buttonsName = ["home", "stake", "roadmap", "rarity"];
   const pathname = usePathname();
   let pathnameString = pathname.replace("/", "");
-  function handleConnectWalletClick(buttonName) {
-    if (buttonName === "connect wallet") {
-      setIsWalletConnected(!isWalletConnected);
-      console.log("connect wallet clicked")
-    }
+  function handleConnectWalletClick() {
+    setIsWalletConnected(!isWalletConnected);
   }
   const handleButtonClick = (buttonName) => {
     setActiveButton(buttonName);
-    handleConnectWalletClick(buttonName);
+    // handleConnectWalletClick(buttonName);
   };
   return (
     <div className={styles.navbar_container}>
-      <Image src={degenKongLogo} quality={100} alt="degen kong logo" />
-      {buttonsName.map((buttonName, i) => (
-        <Button
-          key={i}
-          text={buttonName}
-          isWalletConnected={isWalletConnected}
-          isSelected={buttonName === pathnameString}
-          onClick={() => handleButtonClick(buttonName)}
-        />
-      ))}
+      <div className={styles.navbar_wrap}>
+        <Image src={degenKongLogo} quality={100} alt="degen kong logo" />
+        <div className={styles.navbar_buttons_navigate_container}>
+          {buttonsName.map((buttonName, i) => (
+            <Button
+              key={i}
+              text={buttonName}
+              isWalletConnected={isWalletConnected}
+              isSelected={buttonName === pathnameString}
+              onClick={() => handleButtonClick(buttonName)}
+            />
+          ))}
+        </div>
+        <div>
+          <Button
+            text="connect wallet"
+            onClick={() => handleConnectWalletClick()}
+            isWalletConnected={isWalletConnected}
+          />
+        </div>
+      </div>
     </div>
   );
 }
