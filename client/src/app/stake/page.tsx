ed4374472f7745cc2452c551079534a6aa9e95db
Ra <rami@jwcorp.fr>
2024-01-01 21:40:33 +0100

    style: added medium buttons to stake page + new font

--- client/src/app/stake/page.tsx
+++ client/src/app/stake/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import degenKongWatermark from "../../../public/Assets/image/degenkong-watermark.svg";
 import styles from "../../styles/pages/page-stake.module.css";
 import degenPfp from "../../../public/Assets/image/degenKong-pfp.png";
+import ButtonMedium from "../../components/buttons/ButtonMedium";
 function page() {
   return (
     <>
@@ -14,139 +15,161 @@ function page() {
             alt="degen kong long background"
           />
         </div>
-        <div className={styles.stake_page_first_bloc}>
-          <div className={styles.stake_page_first_bloc_wrap}>
-            <div className={styles.stake_page_image_container}>
-              <Image
-                src={degenPfp}
-                quality={100}
-                alt="degen kong long background"
-              />
+        <div className={styles.stake_page_first_section}>
+          <div className={styles.stake_page_first_section_top_button_container}>
+            <ButtonMedium text="WALLET" />
+          </div>
+          <div className={styles.stake_page_first_bloc}>
+            <div className={styles.stake_page_first_bloc_wrap}>
+              <div className={styles.stake_page_image_container}>
+                <Image
+                  src={degenPfp}
+                  quality={100}
+                  alt="degen kong long background"
+                />
+              </div>
+              <div className={styles.stake_page_image_container}>
+                <Image
+                  src={degenPfp}
+                  quality={100}
+                  alt="degen kong long background"
+                />
+              </div>
+              <div className={styles.stake_page_image_container}>
+                <Image
+                  src={degenPfp}
+                  quality={100}
+                  alt="degen kong long background"
+                />
+              </div>
             </div>
-            <div className={styles.stake_page_image_container}>
-              <Image
-                src={degenPfp}
-                quality={100}
-                alt="degen kong long background"
-              />
-            </div>
-            <div className={styles.stake_page_image_container}>
-              <Image
-                src={degenPfp}
-                quality={100}
-                alt="degen kong long background"
-              />
-            </div>
-            
+          </div>
+          <div
+            className={styles.stake_page_first_section_bottom_buttons_container}
+          >
+            <ButtonMedium text="SELECT ALL (0)" />
+            <ButtonMedium text="STAKE (0)" />
           </div>
         </div>
-        <div className={styles.stake_page_second_bloc}>
-          <div className={styles.stake_page_second_bloc_wrap}>
-            <div className={styles.stake_page_image_container}>
-              <Image
-                src={degenPfp}
-                quality={100}
-                alt="degen kong long background"
-              />
+        <div className={styles.stake_page_second_section}>
+          <div
+            className={styles.stake_page_second_section_buttons_top_container}
+          >
+            <ButtonMedium text="STAKED (3)" />
+            <ButtonMedium text="CLAIM REWARD" />
+          </div>
+          <div className={styles.stake_page_second_bloc}>
+            <div className={styles.stake_page_second_bloc_wrap}>
+              <div className={styles.stake_page_image_container}>
+                <Image
+                  src={degenPfp}
+                  quality={100}
+                  alt="degen kong long background"
+                />
+              </div>
+              <div className={styles.stake_page_image_container}>
+                <Image
+                  src={degenPfp}
+                  quality={100}
+                  alt="degen kong long background"
+                />
+              </div>
+              <div className={styles.stake_page_image_container}>
+                <Image
+                  src={degenPfp}
+                  quality={100}
+                  alt="degen kong long background"
+                />
+              </div>
+              <div className={styles.stake_page_image_container}>
+                <Image
+                  src={degenPfp}
+                  quality={100}
+                  alt="degen kong long background"
+                />
+              </div>
+              <div className={styles.stake_page_image_container}>
+                <Image
+                  src={degenPfp}
+                  quality={100}
+                  alt="degen kong long background"
+                />
+              </div>
+              <div className={styles.stake_page_image_container}>
+                <Image
+                  src={degenPfp}
+                  quality={100}
+                  alt="degen kong long background"
+                />
+              </div>
+              <div className={styles.stake_page_image_container}>
+                <Image
+                  src={degenPfp}
+                  quality={100}
+                  alt="degen kong long background"
+                />
+              </div>
+              <div className={styles.stake_page_image_container}>
+                <Image
+                  src={degenPfp}
+                  quality={100}
+                  alt="degen kong long background"
+                />
+              </div>
+              <div className={styles.stake_page_image_container}>
+                <Image
+                  src={degenPfp}
+                  quality={100}
+                  alt="degen kong long background"
+                />
+              </div>
+              <div className={styles.stake_page_image_container}>
+                <Image
+                  src={degenPfp}
+                  quality={100}
+                  alt="degen kong long background"
+                />
+              </div>
+              <div className={styles.stake_page_image_container}>
+                <Image
+                  src={degenPfp}
+                  quality={100}
+                  alt="degen kong long background"
+                />
+              </div>
+              <div className={styles.stake_page_image_container}>
+                <Image
+                  src={degenPfp}
+                  quality={100}
+                  alt="degen kong long background"
+                />
+              </div>
+              <div className={styles.stake_page_image_container}>
+                <Image
+                  src={degenPfp}
+                  quality={100}
+                  alt="degen kong long background"
+                />
+              </div>
+              <div className={styles.stake_page_image_container}>
+                <Image
+                  src={degenPfp}
+                  quality={100}
+                  alt="degen kong long background"
+                />
+              </div>
+              <div className={styles.stake_page_image_container}>
+                <Image
+                  src={degenPfp}
+                  quality={100}
+                  alt="degen kong long background"
+                />
+              </div>
             </div>
-            <div className={styles.stake_page_image_container}>
-              <Image
-                src={degenPfp}
-                quality={100}
-                alt="degen kong long background"
-              />
-            </div>
-            <div className={styles.stake_page_image_container}>
-              <Image
-                src={degenPfp}
-                quality={100}
-                alt="degen kong long background"
-              />
-            </div>
-            <div className={styles.stake_page_image_container}>
-              <Image
-                src={degenPfp}
-                quality={100}
-                alt="degen kong long background"
-              />
-            </div>
-            <div className={styles.stake_page_image_container}>
-              <Image
-                src={degenPfp}
-                quality={100}
-                alt="degen kong long background"
-              />
-            </div>
-            <div className={styles.stake_page_image_container}>
-              <Image
-                src={degenPfp}
-                quality={100}
-                alt="degen kong long background"
-              />
-            </div>
-            <div className={styles.stake_page_image_container}>
-              <Image
-                src={degenPfp}
-                quality={100}
-                alt="degen kong long background"
-              />
-            </div>
-            <div className={styles.stake_page_image_container}>
-              <Image
-                src={degenPfp}
-                quality={100}
-                alt="degen kong long background"
-              />
-            </div>
-            <div className={styles.stake_page_image_container}>
-              <Image
-                src={degenPfp}
-                quality={100}
-                alt="degen kong long background"
-              />
-            </div>
-            <div className={styles.stake_page_image_container}>
-              <Image
-                src={degenPfp}
-                quality={100}
-                alt="degen kong long background"
-              />
-            </div>
-            <div className={styles.stake_page_image_container}>
-              <Image
-                src={degenPfp}
-                quality={100}
-                alt="degen kong long background"
-              />
-            </div>
-            <div className={styles.stake_page_image_container}>
-              <Image
-                src={degenPfp}
-                quality={100}
-                alt="degen kong long background"
-              />
-            </div>
-            <div className={styles.stake_page_image_container}>
-              <Image
-                src={degenPfp}
-                quality={100}
-                alt="degen kong long background"
-              />
-            </div>
-            <div className={styles.stake_page_image_container}>
-              <Image
-                src={degenPfp}
-                quality={100}
-                alt="degen kong long background"
-              />
-            </div>
-            <div className={styles.stake_page_image_container}>
-              <Image
-                src={degenPfp}
-                quality={100}
-                alt="degen kong long background"
-              />
-            </div>
+          </div>
+          <div className={styles.stake_page_second_bloc_bottom_buttons_container}>
+            <ButtonMedium text="SELECT ALL (0)" />
+            <ButtonMedium text="UNSTAKE(0)" />
           </div>
         </div>
       </div>
